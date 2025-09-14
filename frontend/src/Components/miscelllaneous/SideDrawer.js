4e213a8c1ae8c974c57b3fa776f165bd37abd8b2
himanshusharma0031 <himanshusharma22902@gmail.com>
2025-09-14 18:16:33 +0530

    Update SideDrawer.js

--- frontend/src/Components/miscelllaneous/SideDrawer.js
+++ frontend/src/Components/miscelllaneous/SideDrawer.js
@@ -10,7 +10,7 @@ import ProfileModal from "./ProfileModal";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import ChatLoading from "../ChatLoading";
-import UserListItem from "../useAvatar/userListItem";
+import UserListItem from "../useAvatar/UserListItem";
 import NotificationBadge from "@parthamk/notification-badge";
 
 const SideDrawer = () => {
